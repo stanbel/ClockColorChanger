549c78cb5d0b83b78f7823a67cbebd0ec6018459
StanislavB <stanbel@newmail.ru>
2014-04-04 16:18:52 +0400

    added ability to set color

---
+++ ClockColorChanger/src/com/sb/p1/SettingsActivity.java
@@ -0,0 +1,23 @@
+package com.sb.p1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+import android.preference.PreferenceActivity;
+
+public class SettingsActivity extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.layout.activity_settings);
+	} //onCreate
+
+
+
+}
